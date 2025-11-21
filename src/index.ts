403c3cf499dade6137230ee622f91c992c0931c2
Willian-Gabriel-18 <169216330+WillianGabriel18@users.noreply.github.com>
2025-11-21 10:57:38 -0300

    Commit inicial

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import http from 'node:http';
+import fs from 'node:fs';
+import url from 'node:url';
+import path from 'node:path';
+
+const PORT = 8090;
+
+const server = http.createServer(async (req, res)=>{
+    try {
+        const reqUrl = url.parse(req.url?.toString() as string, true);
+        
+        res.setHeader('Content-Type', 'text/html');
+
+        if (req.method === 'GET') {
+            console.log(reqUrl);
+            
+            if(reqUrl.pathname === '/'){
+                const file = await fs.promises.readFile(path.resolve(import.meta.dirname, 'pages', 'index.html'), { encoding: 'utf8' });
+                
+                res.writeHead(200);
+                res.write(file);
+            }
+            else if(reqUrl.pathname === '/about' || reqUrl.pathname === '/about/'){
+                const file = await fs.promises.readFile(path.resolve(import.meta.dirname, 'pages', 'about.html'), { encoding: 'utf8' });
+                
+                res.writeHead(200);
+                res.write(file);
+            }
+            else if(reqUrl.pathname === '/contact-me' || reqUrl.pathname === '/contact-me/'){
+                const file = await fs.promises.readFile(path.resolve(import.meta.dirname, 'pages', 'contact-me.html'), { encoding: 'utf8' });
+                
+                res.writeHead(200);
+                res.write(file);
+            }
+            else{
+                const file = await fs.promises.readFile(path.resolve(import.meta.dirname, 'pages', '404.html'), { encoding: 'utf8' });
+                
+                res.writeHead(404);
+                res.write(file);
+            }
+        }
+    
+        res.end();
+    } catch (error) {
+        res.statusCode = 404;
+        console.log(error);
+        res.end(error);
+    }
+}); 
+
+server.listen(PORT, 'localhost', ()=>{
+    console.log('Servidor rodando na porta ', PORT);
+})
